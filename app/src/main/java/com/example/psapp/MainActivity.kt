7c35d1152ce3f85536c63b9e3cfaec45f89893fe
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-05-19 21:34:56 -0300

    new_changes

--- app/src/main/java/com/example/psapp/MainActivity.kt
+++ app/src/main/java/com/example/psapp/MainActivity.kt
@@ -4,59 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import com.example.psapp.ui.screen.HomeScreen
-import com.example.psapp.ui.theme.PsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
-        setContent {
-            PsAppTheme {
-                Scaffold(
-                    bottomBar = {
-                        NavigationBar(
-                            containerColor = Color.White,
-                            modifier = Modifier
-                                .navigationBarsPadding()
-                        ) {
-                            NavigationBarItem(
-                                selected = false,
-                                onClick = {},
-                                icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-                                label = { Text("Home") }
-                            )
-                            NavigationBarItem(
-                                selected = false,
-                                onClick = {},
-                                icon = {
-                                    Icon(
-                                        Icons.Default.AccountBox,
-                                        contentDescription = "Account"
-                                    )
-                                },
-                                label = { Text("Account") }
-                            )
-                        }
-                    }
-                ) { innerPadding ->
-                    HomeScreen(Modifier.padding(innerPadding))
-                }
-            }
-        }
+        setContent { HomeScreen() }
     }
 }
